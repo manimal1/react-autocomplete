f51218238bfdc9e60b6576f5d0ea0056a5f64d38
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2022-04-11 14:49:09 +0200

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  base: '/react-autocomplete/',
+  plugins: [react(), tsconfigPaths()],
+});
