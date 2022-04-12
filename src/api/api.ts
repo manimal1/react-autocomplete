32dae90d94c1295f9c8d5fa45b3e18c553f4c58b
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2022-04-12 15:18:32 +0200

    update api call

--- src/api/api.ts
+++ src/api/api.ts
@@ -7,4 +7,4 @@ export interface Movie {
 
 export const mockApiCall: Promise<Movie[]> = new Promise((resolve) => resolve(mockApiResponse.data));
 
-export const getMovieData = (title: string) => fetch(`http://omdbapi.com/?s=${title}&apikey=b23ed79b`);
+export const getMovieData = (title: string) => fetch(`https://omdbapi.com/?s=${title}&apikey=b23ed79b`);
